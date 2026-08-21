5408b093d78007efe18693cfcc27926df0dd76d3
dev-raimundos <devraimundos@outlook.com>
2026-08-21 09:40:56 -0300

    Feat: implementa Result Pattern

--- src/Orion.Application/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Orion.Application
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ src/Orion.Application/Result.cs
@@ -0,0 +1,39 @@
+﻿namespace Orion.Application;
+
+public enum ErrorType
+{
+    None,
+    Validation,
+    Unauthorized,
+    Forbidden,
+    NotFound,
+    Conflict,
+    Unexpected
+}
+
+public class Result<T>
+{
+    public bool IsSuccess { get; }
+    public T? Value { get; }
+    public string? Error { get; }
+    public ErrorType ErrorType { get; }
+
+    private Result(bool isSuccess, T? value, string? error, ErrorType errorType)
+    {
+        IsSuccess = isSuccess;
+        Value = value;
+        Error = error;
+        ErrorType = errorType;
+    }
+
+    public static Result<T> Success(T value)
+    {
+        return new Result<T>(true, value, null, ErrorType.None);
+    }
+
+    public static Result<T> Failure(string error, ErrorType errorType)
+    {
+        return new Result<T>(false, default, error, errorType);
+    }
+
+}
